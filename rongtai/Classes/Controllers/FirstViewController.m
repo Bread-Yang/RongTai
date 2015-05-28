6830739739c24bb79098e9348c7a57961a888378
Rongbin <rbyang@xtremeprog.com>
2015-05-28 10:04:47 +0800

    add ShareSDK and app introduce view

--- rongtai/Classes/Controllers/FirstViewController.m
+++ rongtai/Classes/Controllers/FirstViewController.m
@@ -6,9 +6,14 @@
 //  Copyright (c) 2015年 William-zhang. All rights reserved.
 //
 
-#import "FirstViewController.h"
+#import <ShareSDK/ShareSDK.h>
 
-@interface FirstViewController ()
+#import "FirstViewController.h"
+#import "AppIntrouceView.h"
+
+@interface FirstViewController () <AppIntroduceViewDelegate>
+
+@property AppIntrouceView *introduceView;
 
 @end
 
@@ -17,6 +22,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if (![defaults objectForKey:@"intro_screen_viewed"]) {
+        self.introduceView = [[AppIntrouceView alloc] initWithFrame:self.view.frame];
+        self.introduceView.delegate = self;
+        self.introduceView.backgroundColor = [UIColor greenColor];
+        [self.view addSubview:self.introduceView];
+    }
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:YES];
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,10 +55,94 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - AppIntroduceViewDelegate
+
+- (void)onDoneButtonPressed {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:@"YES"forKey:@"intro_screen_viewed"];
+    [defaults synchronize];
+    [UIView animateWithDuration:1.0 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        self.introduceView.alpha = 0;
+    } completion:^(BOOL finished) {
+        [self.introduceView removeFromSuperview];
+    }];
+}
+
+#pragma mark - View Action
+
 - (IBAction)toBleControllerAction:(id)sender {
     UIStoryboard *secondStoryBoard = [UIStoryboard storyboardWithName:@"Second" bundle:[NSBundle mainBundle]];
     UIViewController *viewController = [secondStoryBoard instantiateViewControllerWithIdentifier:@"ScanVC"];
     [self.navigationController pushViewController:viewController animated:YES];
 }
 
+- (IBAction)shareSDKAction:(id)sender {
+     NSString *imagePath = [[NSBundle mainBundle] pathForResource:@"Intro_Screen_Four" ofType:@"png"];
+    
+    //1、构造分享内容
+    id<ISSContent> publishContent = [ShareSDK content:@"要分享的内容"
+                                       defaultContent:@"在IOS上分享"
+                                                image:[ShareSDK imageWithPath:imagePath]
+                                                title:@"ShareSDK"
+                                                  url:@"http://www.mob.com"
+                                          description:@"这是一条演示信息"
+                                            mediaType:SSPublishContentMediaTypeNews];
+  
+    id<ISSContainer> container = [ShareSDK container];
+    
+    //2、弹出分享菜单
+    [ShareSDK showShareActionSheet:container
+                         shareList:nil
+                           content:publishContent
+                     statusBarTips:YES
+                       authOptions:nil
+                      shareOptions:nil
+                            result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
+                                
+                                //可以根据回调提示用户。
+                                if (state == SSResponseStateSuccess)
+                                {
+                                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享成功"
+                                                                                    message:nil
+                                                                                   delegate:self
+                                                                          cancelButtonTitle:@"OK"
+                                                                          otherButtonTitles:nil, nil];
+                                    [alert show];
+                                }
+                                else if (state == SSResponseStateFail)
+                                {
+                                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                                    message:[NSString stringWithFormat:@"失败描述：%@",[error errorDescription]]
+                                                                                   delegate:self
+                                                                          cancelButtonTitle:@"OK"
+                                                                          otherButtonTitles:nil, nil];
+                                    [alert show];
+                                }
+                            }];
+}
+
+
+- (IBAction)sinaLoginAction:(id)sender {
+    [ShareSDK getUserInfoWithType:ShareTypeSinaWeibo
+                      authOptions:nil
+                           result:^(BOOL result, id<ISSPlatformUser> userInfo, id<ICMErrorInfo> error) {
+         
+                               NSString *uid = [userInfo uid];
+                               NSString *nickName = [userInfo nickname];
+                               
+                               NSString *info = [uid stringByAppendingString:nickName];
+        
+                               if (result) {
+                                   UIAlertView *alertView = [[UIAlertView alloc]
+                                       initWithTitle:@"Hello"
+                                       message:info
+                                       delegate:nil
+                                       cancelButtonTitle:@"知道了"
+                                       otherButtonTitles: nil];
+                                   [alertView show];
+                               }
+         
+     }];
+}
 @end
