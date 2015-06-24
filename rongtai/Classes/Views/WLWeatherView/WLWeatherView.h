934ac43fbee3a218e4b48573627bd8a776b3bb42
MJXin <ufoma1120@outlook.com>
2015-06-24 18:24:47 +0800

    天气预报

---
+++ rongtai/Classes/Views/WLWeatherView/WLWeatherView.h
@@ -0,0 +1,16 @@
+//
+//  WLWeatherView.h
+//  WLWeatherView
+//
+//  Created by William-zhang on 15/6/24.
+//  Copyright (c) 2015年 William-zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface WLWeatherView : UIView
+
+@property(nonatomic, strong)CLLocationManager* lManager;
+
+@end
