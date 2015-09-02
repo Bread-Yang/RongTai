f3a6a25eaff1ed77845f9091391a094bc4ed263d
yoghourt <yoghourt@Yoghourts-MacBook.local>
2015-09-02 14:55:52 +0800

    fix timing plan bugs

---
+++ rongtai/Classes/Categories/UIImageView+RT.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView.h
+//  rongtai
+//
+//  Created by yoghourt on 9/2/15.
+//  Copyright (c) 2015 William-zhang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImageView : NSObject
+
+@end
---
+++ rongtai/Classes/Categories/UIImageView+RT.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView.m
+//  rongtai
+//
+//  Created by yoghourt on 9/2/15.
+//  Copyright (c) 2015 William-zhang. All rights reserved.
+//
+
+#import "UIImageView.h"
+
+@implementation UIImageView
+
+@end
