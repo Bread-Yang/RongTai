e58d48dd62bc6a8df52733f96966802e974f6d51
MJXin <ufoma1120@outlook.com>
2015-09-10 15:03:53 +0800

    bug修复

--- rongtai/Classes/Controllers/MainViewController.m
+++ rongtai/Classes/Controllers/MainViewController.m
@@ -624,11 +624,8 @@
 			dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
 			
 			dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-				
 				[self jumpToCorrespondingControllerByMassageStatus];
-				
 			});
-			
 		}
 	}
 }
